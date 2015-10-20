eaeff17f609ee1fab6a27718f41c3f4176af1bf7
Toma Luca <toma.luca95@gmail.com>
2015-10-20 19:07:14 +0000

    update single node startup script

--- make-single-node.sh
+++ make-single-node.sh
@@ -10,7 +10,7 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
  --privileged -p 80:80 -p 443:443 -dti --restart=always \
  -e DOCKERAPI_HOSTNAME=$DOCKERAPI_HOSTNAME \
  -e DOCKERAPI_HOMEDIRS=$DOCKERAPI_HOMEDIRS \
- wikitolearndockeraccess/virtualfactory:0.5
+ wikitolearndockeraccess/virtualfactory:0.6
 
 while ! docker exec -ti virtualfactory wget -o /dev/null -O - http://127.0.0.1:5000/pull_images ; do
  sleep 1
